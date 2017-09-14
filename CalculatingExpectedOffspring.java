c63ddfca079dfffe6350a93a9c7296c373e8066a
Ahmed Youssef <ahmedalaa@windowslive.com>
2017-09-14 14:14:23 -0400

    Calculating Expected Offspring
    http://rosalind.info/problems/iev/
    Topics: Heredity, Probability

--- CalculatingExpectedOffspring.java
+++ CalculatingExpectedOffspring.java
@@ -13,7 +13,7 @@ public class CalculatingExpectedOffspring {
 		for (int i = 0; i < s.length; i++) 
 			input[i] = Integer.parseInt(s[i]);		
 		
-		System.out.println( 	2* (	input[0] + input[1]	+ input[2] + input[3]*0.75 + input[4]*0.5	)	);
+		System.out.println( 2* (input[0] + input[1] + input[2] + input[3]*0.75 + input[4]*0.5) );
 	}
 
 }
