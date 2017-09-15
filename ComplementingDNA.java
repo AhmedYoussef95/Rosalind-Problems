9fd0a46d9eef9d5739e824c4b7e3367af91b1c9c
Ahmed Youssef <ahmedalaa@windowslive.com>
2017-09-14 20:47:06 -0400

    Complementing a Strand of DNA
    Topics: String Algorithms
    Solved on: Jan. 26, 2016, 5:53 p.m.

--- ComplementingDNA.java
+++ ComplementingDNA.java
@@ -7,14 +7,13 @@ import java.util.Scanner;
 public class ComplementingDNA {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		System.out.println(output(s));
-
+		String dna = sc.nextLine();
+		System.out.println(output(dna));
 	}
 
 	public static String output(String dna){
 		String reverse = "";
-		for(int i = dna.length()-1;i >= 0; i--)
+		for(int i = dna.length()-1; i >= 0; i--)
 			reverse += dna.charAt(i);
 		String result = "";
 		for (int i = 0; i < reverse.length(); i++) {
